89d62a82a56a46e786af189f04817261a9251edf
raysr <raysamram@gmail.com>
2019-04-19 12:45:24 +0100

    correcting init

--- app.py
+++ app.py
@@ -4,38 +4,18 @@
 
 from flask import Flask, render_template, request
 # from flask.ext.sqlalchemy import SQLAlchemy
-import logging
-from logging import Formatter, FileHandler
-from forms import *
+
 import os
 
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="static")
 app.config.from_object('config')
 #db = SQLAlchemy(app)
 
-# Automatically tear down SQLAlchemy.
-'''
-@app.teardown_request
-def shutdown_session(exception=None):
-    db_session.remove()
-'''
 
-# Login required decorator.
-'''
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('login'))
-    return wrap
-'''
 #----------------------------------------------------------------------------#
 # Controllers.
 #----------------------------------------------------------------------------#
@@ -44,11 +24,6 @@ def login_required(test):
 @app.route('/')
 def home():
     return render_template('pages/home.html')
-
-
-@app.route('/about')
-def about():
-    return render_template('pages/placeholder.about.html')
 
 
 @app.route('/login')
